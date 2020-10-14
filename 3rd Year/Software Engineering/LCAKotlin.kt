1acd8a472e5289fd2e8f2cb512bc6e60d34862ee
John Burke <burkejohn1999@gmail.com>
2020-10-14 13:01:41 +0100

    Creating BT and addValue function

--- 3rd Year/Software Engineering/LCAKotlin.kt
+++ 3rd Year/Software Engineering/LCAKotlin.kt
@@ -1,11 +1,56 @@
 class Node(input: Int) {
-    val left: Node? = null
-    val right: Node? = null
+    var left: Node? = null
+    var right: Node? = null
     val value: Int = input
 }
 
-val root = Node(1)
+class BT(input: Int) {
+    val root: Node? = Node(input)
+
+    fun addValue(value: Int){
+        var newNode = Node(value)
+        var node: Node? = root
+        while (node != null){
+            if (value == node.value){
+                node = null
+            }
+            else if(value < node.value){
+                if(node.left == null){
+                    node.left = newNode
+                    node = null
+                }
+                else{
+                    node = node.left
+                }
+            }
+            else {
+                if (node.right == null){
+                    node.right = newNode
+                    node = null
+                }
+                else {
+                    node = node.right
+                }
+            }
+        }
+    }
+}
+
+
+
 
 fun main(){
-    println(root.value)
+    
+    var bt = BT(10)
+    bt.addValue(9)
+    bt.addValue(11)
+    bt.addValue(8)
+    bt.addValue(15)
+    bt.addValue(12)
+    bt.addValue(1)
+    bt.addValue(7)
+    bt.addValue(-5)
+    bt.addValue(200)
+    bt.addValue(5)
+    println(bt.root)
 }
