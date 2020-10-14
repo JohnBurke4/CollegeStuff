21265b598312375c552ddff5ecd72c03204b158a
John Burke <burkejohn1999@gmail.com>
2020-10-14 12:52:43 +0100

    Creating node class

--- 3rd Year/Software Engineering/LCAKotlin.kt
+++ 3rd Year/Software Engineering/LCAKotlin.kt
@@ -1,3 +1,11 @@
+class Node(input: Int) {
+    val left: Node? = null
+    val right: Node? = null
+    val value: Int = input
+}
+
+val root = Node(1)
+
 fun main(){
-    println("Hello World!");
+    println(root.value)
 }
