81f196d4e3ec94c03ca9f660625b123946b5d1df
John Burke <burkejohn1999@gmail.com>
2020-11-09 13:49:18 +0000

    Fixing graph

--- 3rd Year/Software Engineering/LCAPython.py
+++ 3rd Year/Software Engineering/LCAPython.py
@@ -7,23 +7,24 @@ class Node:
 
     def __init__(self, value):
         self.value = value
+        self.children = []
 
 
 class Graph:
     root = None
 
     def __init__(self, value):
-        self.root = new Node(value)
+        self.root = Node(value)
 
     def contains(self, value, node):
         if (node.value == value):
-            return true
+            return True
 
         for child in node.children:
             if (self.contains(value, child)):
-                return true
+                return True
 
-        return false
+        return False
 
     def get(self, value, node):
         if (node.value == value):
@@ -41,73 +42,77 @@ class Graph:
             return
         else:
             node = self.get(parent, self.root)
+            print(node.value)
             if (node != None):
                 newNode = Node(value)
                 node.children.append(newNode)
 
-    def ancestors(self, value, node, array):
+    # def ancestors(self, value, node, array):
+    #     if (node == None):
+    #         return array
+
+    #     elif (node.value == value):
+    #         array.append(value)
+    #         return array
+    #     elif (array == []):
+    #         array = self.ancestors(value, node.left, array)
+    #         if (array == []):
+    #             array = self.ancestors(value, node.right, array)
+    #         if (array != []):
+    #             array.append(node.value)
+    #         return array
+    #     else:
+    #         return array
+
+    # def lCA(self, valueA, valueB):
+    #     ancestorsA = self.ancestors(value=valueA, node=self.root, array=[])
+    #     ancestorsB = self.ancestors(valueB, self.root, [])
+    #     for value1 in ancestorsA:
+    #         for value2 in ancestorsB:
+    #             if (value1 == value2):
+    #                 return value1
+
+    def print(self):
+        return self.printNode(self.root, "")
+
+    def printNode(self, node, prefix):
         if (node == None):
-            return array
-
-        elif (node.value == value):
-            array.append(value)
-            return array
-        elif (array == []):
-            array = self.ancestors(value, node.left, array)
-            if (array == []):
-                array = self.ancestors(value, node.right, array)
-            if (array != []):
-                array.append(node.value)
-            return array
-        else:
-            return array
-
-    def lCA(self, valueA, valueB):
-        ancestorsA = self.ancestors(value=valueA, node=self.root, array=[])
-        ancestorsB = self.ancestors(valueB, self.root, [])
-        for value1 in ancestorsA:
-            for value2 in ancestorsB:
-                if (value1 == value2):
-                    return value1
-
-    def prettyPrintValues(self):
-        return self.prettyPrint(root, "")
-
-    def prettyPrint(self, x, prefix):
-        if (x == None):
-            return prefix + "-null\n"
-        else:
-            return prefix + "-" + str(x.value) + "\n" + self.prettyPrint(x.left, prefix + " |") + self.prettyPrint(x.right, prefix + "  ")
+            return ""
+        result = prefix + str(node.value) + "\n"
+        for child in node.children:
+            print(child)
+            result += self.printNode(child, prefix + "+")
+        return result
 
 
-class TestBTMethods(unittest.TestCase):
+# class TestBTMethods(unittest.TestCase):
 
-    def testCreation(self):
-        root = Node(10)
-        bt = BT(root)
-        self.assertEqual(bt.root.value, 10)
-        self.assertIsNone(bt.root.left)
-        self.assertIsNone(bt.root.right)
+#     def testCreation(self):
+#         root = Node(10)
+#         bt = BT(root)
+#         self.assertEqual(bt.root.value, 10)
+#         self.assertIsNone(bt.root.left)
+#         self.assertIsNone(bt.root.right)
 
-    def testLCA(self):
-        root = Node(10)
-        bt = BT(root)
-        bt.addValue(5)
-        bt.addValue(15)
-        bt.addValue(4)
-        bt.addValue(6)
-        bt.addValue(12)
-        bt.addValue(11)
-        bt.addValue(14)
-        bt.addValue(13)
+#     def testLCA(self):
+#         root = Node(10)
+#         bt = BT(root)
+#         bt.addValue(5)
+#         bt.addValue(15)
+#         bt.addValue(4)
+#         bt.addValue(6)
+#         bt.addValue(12)
+#         bt.addValue(11)
+#         bt.addValue(14)
+#         bt.addValue(13)
 
-        self.assertEqual(bt.lCA(5, 15), 10)
-        self.assertEqual(bt.lCA(4, 13), 10)
-        self.assertEqual(bt.lCA(4, 6), 5)
-        self.assertEqual(bt.lCA(5, 4), 5)
-        self.assertEqual(bt.lCA(13, 11), 12)
-        self.assertEqual(bt.lCA(14, 15), 15)
+#         self.assertEqual(bt.lCA(5, 15), 10)
+#         self.assertEqual(bt.lCA(4, 13), 10)
+#         self.assertEqual(bt.lCA(4, 6), 5)
+#         self.assertEqual(bt.lCA(5, 4), 5)
+#         self.assertEqual(bt.lCA(13, 11), 12)
+#         self.assertEqual(bt.lCA(14, 15), 15)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
