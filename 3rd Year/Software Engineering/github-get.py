8f1b31e8c53e76dd85bf64851f2f4d79ca336986
John Burke <burkejohn1999@gmail.com>
2020-12-14 13:30:02 +0000

    Adding user input

--- 3rd Year/Software Engineering/github-get.py
+++ 3rd Year/Software Engineering/github-get.py
@@ -1,10 +1,14 @@
 import requests
 
-repoName = input("Please input a repo name: ")
+repoOwner = input("Please input the repo owner: ")
+repoName = input("Please input the repo name: ")
+
 f = open("pass.txt", "r")
 authCode = str(f.read())
-print(authCode)
+
 headers = {"Authorization": "token %s" % authCode}
-requestURL = "https://api.github.com/repos/torvalds/linux/commits"
+requestURL = "https://api.github.com/repos/%s/%s/commits" % repoOwner, repoName
+
 r = requests.get(requestURL, headers=headers)
+
 print(r.text)
