fadc7bb7c2e98d918b7b728cc68b724bc6e339e3
John Burke <burkejohn1999@gmail.com>
2020-12-14 14:04:21 +0000

    Printing out results

--- 3rd Year/Software Engineering/github-get.py
+++ 3rd Year/Software Engineering/github-get.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 repoOwner = input("Please input the repo owner: ")
 repoName = input("Please input the repo name: ")
+loops = int(input("How many pages do you want to check: "))
 
 f = open("pass.txt", "r")
 authCode = str(f.read())
@@ -15,14 +16,23 @@ requestURL = "https://api.github.com/repos/%s/%s/commits" % (
 r = requests.get(requestURL, headers=headers)
 
 resultJSON = json.loads(r.text)
-print(r.links["next"]["url"])
 timeDict = {}
-for i in resultJSON:
-    date = datetime.strptime(
-        i["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
-    if date.hour in timeDict:
-        timeDict[date.hour] += 1
-    else:
-        timeDict[date.hour] = 1
+counter = 0
+while (counter != loops - 1 and r.status_code == 200):
+    print("Page: %s" % str(counter))
+    for i in resultJSON:
+        date = datetime.strptime(
+            i["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+        if date.hour in timeDict:
+            timeDict[date.hour] += 1
+        else:
+            timeDict[date.hour] = 1
 
-print(timeDict)
+    requestURL = r.links["next"]["url"]
+    r = requests.get(requestURL, headers=headers)
+    resultJSON = json.loads(r.text)
+    counter += 1
+
+for i in range(0, 24):
+    if i in timeDict:
+        print("%s: %s" % (str(i), timeDict[i]))
