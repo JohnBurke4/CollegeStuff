59dc4cbd1272e5e76f803a7349aa71f54b00eed1
John Burke <burkejohn1999@gmail.com>
2020-12-27 17:03:46 +0000

    Finishing git get commits in js

--- 3rd Year/Software Engineering/github-visualisation/src/api/getCommits.js
+++ 3rd Year/Software Engineering/github-visualisation/src/api/getCommits.js
@@ -2,22 +2,30 @@ async function getCommits(repoOwner, repoName, authToken){
     const header = new Headers({
         'Authorization': `token ${authToken}` 
      });
-
-    let url = `https://api.github.com/repos/${repoOwner}/${repoName}/commits`;
     
-    let maxPages = 1;
-    let currentPage = 0;
-    let result = {}
+    let maxPages = 10;
+    let currentPage = 1;
+    let result = new Array(24).fill(0);
     while (currentPage < maxPages){
+        let url = `https://api.github.com/repos/${repoOwner}/${repoName}/commits?per_page=100&page=${currentPage}`
         var data = await fetch(url, 
             {
                 method: 'get',
                 headers: header
             });
+        let json = await data.json();
+        let length = json.length;
+        for(let i = 0; i < length; i++){
+            let date = new Date(json[i]["commit"]["committer"]["date"]);
+            let hours = date.getHours();
+            result[hours]++;
+        }
+
+        
         currentPage++;
-    let json = await data.json();
-    
-    console.log(json);
+    }
+    for(let i = 0; i < 24; i++){
+        console.log("Hour " + i + ": " +  result[i] + " commits");
     }
     return result;
 }
