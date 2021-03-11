df461ace2e3904da2ca9a23f64a1d95948702fe2
John Burke <burkejohn1999@gmail.com>
2021-03-11 14:44:05 +0000

    Adding proxy files

---
+++ 3rd Year/Concurrent Systems/gui.py
@@ -0,0 +1,114 @@
+import tkinter as tk
+import threading
+from proxy import Proxy
+
+
+class GUI:
+
+    config = {
+        'HOST_NAME': '0.0.0.0',
+        'BIND_PORT': 12345,
+        'MAX_REQUEST_LENGTH': 8192,
+        'CONNECTION_TIMEOUT': 5,
+        'TTL': 10080
+    }
+
+    def __init__(self):
+
+        self.proxyStarted = False
+        self.m = tk.Tk()
+        self.m.geometry('900x600')
+        self.currentBlacklistUrl = tk.StringVar()
+
+        self.timeSaved = tk.StringVar()
+        self.timeSaved.set('Time saved by cache: 0s')
+        self.bandwidthSaved = tk.StringVar()
+        self.bandwidthSaved.set('Bandwidth saved by cache:\n 0 KB')
+
+        self.proxy = Proxy(self.config, self.timeSaved, self.bandwidthSaved)
+
+        self.leftScreenSide = tk.Frame(self.m)
+
+        self.startProxyButton = tk.Button(
+            self.leftScreenSide, text='Start Proxy', width='25', command=lambda: self.startProxy(), state=tk.NORMAL)
+        self.startProxyButton.pack(side='top', padx='5', pady='5')
+
+        self.stopProxyButton = tk.Button(
+            self.leftScreenSide, text='Stop Proxy', width='25', command=lambda: self.stopProxy(), state=tk.DISABLED)
+        self.stopProxyButton.pack(side='top', padx='5')
+
+        self.addToBlacklistButton = tk.Button(
+            self.leftScreenSide, text='Add to blacklist', width='25', command=lambda: self.addToBlacklist())
+        self.addToBlacklistButton.pack(side='top', padx='5', pady='5')
+
+        self.removeFromBlacklistButton = tk.Button(
+            self.leftScreenSide, text='Remove from blacklist', width='25', command=lambda: self.removeFromBlacklist())
+        self.removeFromBlacklistButton.pack(side='top', padx='5')
+
+        self.blacklistEntry = tk.Entry(
+            self.leftScreenSide, width='30', textvariable=self.currentBlacklistUrl)
+        self.blacklistEntry.pack(side='top', padx='5', pady='5')
+
+        self.blacklistText = tk.Label(
+            self.leftScreenSide, width='30', text='Blacklist')
+        self.blacklistText.pack(side='top', padx='5')
+
+        self.leftScreenSide.pack(side='left', fill=tk.Y)
+
+        self.blacklistBox = tk.Listbox(
+            self.leftScreenSide, width=10, selectmode=tk.SINGLE)
+        self.blacklistBox.pack(fill='both', expand=1, side='top')
+
+        self.cacheTimeSavedText = tk.Label(
+            self.leftScreenSide, width='30', textvariable=self.timeSaved)
+        self.cacheTimeSavedText.pack(side='top', padx='5', pady='5')
+
+        self.cacheBandwidthSavedText = tk.Label(
+            self.leftScreenSide, width='30', textvariable=self.bandwidthSaved)
+        self.cacheBandwidthSavedText.pack(side='bottom', padx='5', pady='5')
+
+        self.proxyResponseBox = tk.Listbox(self.m, width=10)
+        self.proxyResponseBox.pack(fill='both', expand=2, side='right')
+
+        self.m.mainloop()
+
+    def startProxy(self):
+        if (not self.proxyStarted):
+            self.proxy.initialise()
+            self.proxyStarted = True
+            self.startProxyButton['state'] = tk.DISABLED
+            self.stopProxyButton['state'] = tk.NORMAL
+            self.proxyResponseBox.insert(tk.END, 'Starting proxy')
+            process = threading.Thread(
+                target=lambda: self.proxy.listenForClients(self.proxyResponseBox))
+            process.start()
+
+    def stopProxy(self):
+        if (self.proxyStarted):
+            self.proxyStarted = False
+            self.startProxyButton['state'] = tk.NORMAL
+            self.stopProxyButton['state'] = tk.DISABLED
+            self.proxyResponseBox.insert(tk.END, 'Shutting down proxy')
+            self.proxy.shutdown()
+
+    def addToBlacklist(self):
+        url = self.currentBlacklistUrl.get()
+        self.proxy.addToBlacklist(url)
+        self.blacklistBox.insert(tk.END, url)
+        self.blacklistEntry.delete(0, 'end')
+
+    def removeFromBlacklist(self):
+
+        index = self.blacklistBox.curselection()
+        if (index != ()):
+            url = self.blacklistBox.get(index[0])
+            self.proxy.removeFromBlacklist(url)
+            self.blacklistBox.delete(index)
+
+
+def main():
+    GUI()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 3rd Year/Concurrent Systems/proxy.py
@@ -0,0 +1,252 @@
+import signal
+import socket
+import threading
+import sys
+import tkinter as tk
+import ssl
+import time
+
+
+class Proxy:
+
+    timeSaved = 0.0
+    bandwidthSaved = 0
+
+    blockedHTML = """
+    <html>
+        <head>
+        </head>
+
+        <body>
+            <h1>This website is blocked by the proxy</h1>
+        </body>
+
+    </html>
+
+    """
+
+    def __init__(self, config, timeText, bandwidthText):
+        self.timeText = timeText
+        self.bandwidthText = bandwidthText
+        self.config = config
+        self.blacklist = set()
+        self.cache = {}
+
+    def initialise(self):
+
+        # Shutdown on Ctrl+C
+        signal.signal(signal.SIGINT, self.shutdown)
+
+        # Create a TCP socket
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Re-use the socket
+        self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind socket to a public host, and a port
+        self.serverSocket.bind(
+            (self.config['HOST_NAME'], self.config['BIND_PORT']))
+
+        self.serverSocket.listen(100)  # Server Socket
+        self.__clients = {}
+
+    def listenForClients(self, textBox):
+        self.textBox = textBox
+        while True:
+            # Esatblish the connection
+            (clientSocket, clientAddress) = self.serverSocket.accept()
+
+            d = threading.Thread(name=self.getClientName(clientAddress),
+                                 target=self.proxyThread, args=(clientSocket, clientAddress))
+
+            d.setDaemon(True)
+            d.start()
+        self.shutdown(0, 0)
+
+    def proxyThread(self, clientSocket, clientAddress):
+        # Get the request from the browser
+        request = clientSocket.recv(self.config['MAX_REQUEST_LENGTH'])
+        startTime = time.time()
+
+        try:
+            # Parse the first line
+            firstLine = request.decode().split('\n')[0]
+            if (firstLine != ''):
+                self.textBox.insert(tk.END, request.decode())
+                url = firstLine.split(' ')[1]
+
+                # Find pos of ://
+                httpsPos = url.find('://')
+
+                if (httpsPos == -1):
+                    temp = url
+                else:
+                    temp = url[(httpsPos+3):]
+
+                # Find port position
+                portPos = temp.find(':')
+
+                # Find end of web server
+                webserverPos = temp.find('/')
+
+                if (webserverPos == -1):
+                    webserverPos = len(temp)
+
+                webserver = ''
+
+                port = -1
+                if (portPos == -1 or webserverPos < portPos):
+                    # Default port
+                    port = 80
+                    webserver = temp[:webserverPos]
+
+                else:
+                    # Specific port
+                    port = int((temp[(portPos+1):])[:webserverPos-portPos-1])
+                    webserver = temp[:portPos]
+
+                if (webserver in self.blacklist):
+                    self.textBox.insert(tk.END, "This web address is blocked")
+                    clientSocket.sendall(self.blockedHTML.encode())
+
+                    clientSocket.close()
+                    return
+
+                cache = self.getFromCache(firstLine)
+                if (cache is not None):
+                    bandwidth = 0
+                    for data in cache[1]:
+                        bandwidth += len(data)
+                        clientSocket.sendall(data)
+
+                    endTime = time.time()
+                    timeElapsed = endTime-startTime
+                    currentTimeSaved = cache[2] - timeElapsed
+                    self.timeSaved += currentTimeSaved
+                    self.bandwidthSaved += bandwidth
+                    self.textBox.insert(
+                        tk.END, 'Time saved by fetching from cache: {:.5f}'.format(currentTimeSaved))
+                    self.textBox.insert(
+                        tk.END, 'Bandwidth saved by fetching from cache: {:.5f} KB/s'.format(
+                            bandwidth / (currentTimeSaved * 1024)))
+                    self.timeText.set(
+                        'Time saved by caching: {:.5f}s'.format(self.timeSaved))
+                    self.bandwidthText.set(
+                        'Network traffic saved by caching:\n {:.3f} KB'.format(self.bandwidthSaved / 1024))
+                    clientSocket.close()
+                    return
+
+                try:
+
+                    # Connect to destination server
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.settimeout(self.config['CONNECTION_TIMEOUT'])
+
+                    if ("CONNECT" in firstLine):
+
+                        s.connect((webserver, port))
+                        # Setting up http tunneling reply
+                        reply = "HTTP/1.0 200 Connection established\r\n"
+                        reply += "Proxy-agent: MyTestProxy\r\n"
+                        reply += "\r\n"
+
+                        clientSocket.sendall(reply.encode())
+
+                        # Setting up websocket connection
+                        clientSocket.setblocking(0)
+                        s.setblocking(0)
+
+                        while True:
+                            try:
+                                # Recieving new request
+                                newRequest = clientSocket.recv(
+                                    self.config['MAX_REQUEST_LENGTH'])
+                                # Sending request to the server
+                                s.sendall(newRequest)
+                            except socket.error as err:
+                                pass
+                            try:
+                                # Recieving reply from the server
+                                reply = s.recv(
+                                    self.config['MAX_REQUEST_LENGTH'])
+
+                                # Sending reply to the client
+                                clientSocket.sendall(reply)
+                            except socket.error as err:
+                                pass
+
+                    else:
+                        s.connect((webserver, port))
+
+                        clientSocket.setblocking(0)
+                        s.setblocking(0)
+
+                        s.sendall(request)
+
+                        cache = []
+
+                        while True:
+                            try:
+                                # Recieve data from web serer
+                                data = s.recv(
+                                    self.config['MAX_REQUEST_LENGTH'])
+
+                                if (len(data) == self.config['MAX_REQUEST_LENGTH']):
+                                    cache.append(data)
+                                    clientSocket.send(data)
+                                elif (len(data) > 0):
+                                    cache.append(data)
+                                    clientSocket.send(data)
+                                    endTime = time.time()
+                                    self.addToCache(
+                                        firstLine, cache, (endTime - startTime))
+                                else:
+
+                                    break
+                            except:
+                                pass
+
+                    s.close()
+                    clientSocket.close()
+                except socket.error as errorMsg:
+                    print('ERROR: ' + str(errorMsg))
+                    if s:
+                        s.close()
+                    if clientSocket:
+                        clientSocket.close()
+            else:
+                clientSocket.close()
+        except:
+            pass
+
+    def shutdown(self):
+        # Close existing server
+        self.serverSocket.close()
+
+    def getClientName(self, clientAddress):
+        return str(clientAddress)
+
+    def addToBlacklist(self, url):
+        self.blacklist.add(url)
+
+    def removeFromBlacklist(self, url):
+        self.blacklist.remove(url)
+
+    def getFromCache(self, request):
+        if ('CONNECT' in request):
+            return None
+        cached = self.cache.get(request)
+        if (cached is not None):
+            if (cached[0] + self.config['TTL'] > int(time.time())):
+                return cached
+        return None
+
+    def addToCache(self, request, data, requestTime):
+        try:
+            self.cache[request] = (
+                int(time.time()),
+                data,
+                requestTime
+            )
+        except:
+            print('something went wrong')
